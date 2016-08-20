30f967f3bb6fb1cd5d1c58ad5c901033ad1f17e3
Jana Whitta <Jana@Janas-MacBook-Pro.local>
2016-08-20 21:15:35 +1200

    Work on displaying price with release

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,12 @@ var express = require('express')
 // var knex = require('knex')(development)
 var token =  'SruQyLPLfHFHGbtBrccWVtSDXydYiLlYEVnHSKsW'
 var Discogs = require ('disconnect').Client
+var Promise = require('promise');
 
 module.exports = {
   get: get,
   post: post,
-  getRelease:getRelease
+  //getAveragePrice: getAveragePrice
 }
 
 
@@ -25,33 +26,36 @@ function get (req, res) {
 
 
 function post (req, res) {
-  var id = req.body.id
-  var db = new Discogs({userToken:token}).database()
-  db.search(id, function (err, releaseData) {
-    console.log(err)
-    console.log (releaseData.results[0].format)
+  var catalogueNumber = req.body.id
+  var db = new Discogs({userToken: token}).database()
+  db.search(catalogueNumber, function (err, releaseData) {
+    if (err) console.log(err)
     if (releaseData) {
-      res.render('index', {releaseData:releaseData.results})
+      var releases = releaseData.results
+      getAveragePrice(releases).then(function(result) {
+        res.render('index', {releaseData: result})
+      }, function(err) {
+        console.log(err);
+      });
     }
   })
 }
 
-  function getRelease (req, res) {
-    var id = req.params.id
-    var marketplace = new Discogs({userToken:token}).marketplace()
-    marketplace.getPriceSuggestions (id, function (err, priceData) {
-      console.log (err)
-      // console.log(priceData)
-      var prices= 0
-      var count= 0
-      for (var key in priceData) {
-        prices += priceData[key].value
-        // console.log (priceData[key].value)
-        count ++
-      }
-      var averagePrice= (prices / count).toFixed(2)
-      // {Math.round(this.getCelsius(this.props.temp), -1)}
-      console.log (averagePrice)
-    })
-
-  }
+function getAveragePrice (releases) {
+  return new Promise(function (resolve, reject) {
+    for (var ii = 0; ii < releases.length; ii++) {
+      var marketplace = new Discogs({userToken: token}).marketplace()
+      marketplace.getPriceSuggestions(releases[ii].id, function (err, priceData) {
+        if (err) console.log (err)
+        var prices = 0
+        var count = 0
+        for (var key in priceData) {
+          prices += priceData[key].value
+          count++
+        }
+        //releases[ii].price = (prices / count).toFixed(2)
+      })
+    }
+    resolve(releases)
+  });
+}
